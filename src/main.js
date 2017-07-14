fe996343e4c0f7d195eed53440202fc4a36d85ac
YiDao <YiDao@YiDaodeMacBook-Pro.local>
2017-07-14 11:11:44 +0800

    iOS Safari - Disable Zoom

--- src/main.js
+++ src/main.js
@@ -8,6 +8,25 @@ import FastClick from 'fastclick'
 
 FastClick.attach(document.body)
 
+// 一些兼容性代码
+/* for iOS 10, users can now pinch-to-zoom even when a website sets user-scalable=no in the viewport. */
+document.documentElement.addEventListener('touchstart', (e) => {
+    if (e.touches.length > 1) {
+        e.preventDefault()
+    }
+}, false)
+
+/* iOS Safari - Disable double click to zoom */
+let lastTouchEnd = 0;
+document.documentElement.addEventListener('touchend', (e) => {
+    let now = (new Date()).getTime()
+    if (now - lastTouchEnd < 300) {
+        e.preventDefault()
+    }
+    lastTouchEnd = now
+}, false)
+
+
 const routes = [{
         path: '/game',
         component: game
